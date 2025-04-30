ece059041739ac2c123a8c1e2400284809af4d8f
Ivan Espinoza <edgarivanespinoza@gmail.com>
2025-05-01 00:39:59 +0200

    Add Web Search, Update LLM & fix Chat Styles

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,25 +1,21 @@
-import { Configuration, OpenAIApi } from 'openai-edge';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(config);
-export const runtime = 'edge';
+export async function POST(req: Request) {
+  const { messages } = await req.json();
 
-export async function POST(request: Request) {
-  let { messages } = await request.json();
-
-  const response = await openai.createChatCompletion({
-    model: 'gpt-4',
-    stream: true,
+  const result = streamText({
+    model: openai.responses('gpt-4o-mini'),
     messages,
-    max_tokens: 500, // Maximo de tokens a devolver
-    temperature: 0.8, // Entre 0 y 1, entre mas alto mas creativo
-    top_p: 1, // Entre 0 y 1, entre mas alto mas creativo
-    frequency_penalty: 1, // Entre -2 y 2, entre mas alto menos repetitivo
-    presence_penalty: 1, // Entre -2 y 2, entre mas alto menos repetitivo
+    tools: {
+      web_search_preview: openai.tools.webSearchPreview(),
+    },
+    maxTokens: 5000, // Maximo de tokens a devolver
+    temperature: 0.6, // Entre 0 y 1, entre mas alto mas creativo
+    topP: 1, // Entre 0 y 1, entre mas alto mas creativo
+    frequencyPenalty: 1, // Entre -2 y 2, entre mas alto menos repetitivo
+    presencePenalty: 1, // Entre -2 y 2, entre mas alto menos repetitivo
   });
-  const stream = OpenAIStream(response);
-  return new StreamingTextResponse(stream);
+
+  return result.toDataStreamResponse();
 }
