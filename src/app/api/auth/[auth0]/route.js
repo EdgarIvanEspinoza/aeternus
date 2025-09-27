da9bc8d83750c928fb6bba5a1ff5757f60a64de2
Ivan Espinoza <edgarivanespinoza@gmail.com>
2025-09-27 02:10:23 +0200

    Refactor authentication route

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -1,9 +1,17 @@
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
 export const GET = handleAuth({
   async login(req, res) {
+    const host = req.headers.get("host") || "";
+
+    const protocol = host.includes("localhost") ? "http" : "https";
+    const redirectUri = `${protocol}://${host}/api/auth/callback`;
+
+    console.log("REDIRECT_URI:", redirectUri);
+
     return handleLogin(req, res, {
-      returnTo: '/chat',
+      returnTo: "/chat",
+      redirectUri,
     });
   },
 });
